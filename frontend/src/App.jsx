8a6d1e1dbb031a059010e50da7c8a4ec5eb46ff2
Null-Helix <jnkercher7@gmail.com>
2024-03-04 20:08:05 -0600

    Add voltage and capacity graphs to frontend tool

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   const [hostname, setHostname] = useState('44-4b-5d-01-04-19')
   const [device, setDevice] = useState('Hub')
+  const [stat, setStat] = useState('Temperature')
   const hostnames = [
     {value: '44-4b-5d-01-04-19', label: '44-4b-5d-01-04-19'},
     {value: '44-4b-5d-01-04-41', label: '44-4b-5d-01-04-41'},
@@ -28,6 +29,11 @@ function App() {
     {value: 'SPO2', label: 'SPO2 Sensor'},
     {value: 'RESP', label: 'RESP Sensor'}
   ]
+  const stats = [
+    {value: 'Temperature', label: 'Temperature'},
+    {value: 'Capacity', label: 'Capacity'},
+    {value: 'Voltage', label: 'Voltage'}
+  ]
   document.body.style.backgroundColor="#640acc"
   return (
     <>
@@ -37,26 +43,37 @@ function App() {
         <h1 style={{fontFamily: 'Source Sans Pro', fontSize: 48}}>Wireless Battery Analytics</h1>
       </div>
       <div style={{borderRadius: 10, backgroundColor: 'white', height: 800, width: 1200, display: 'flex'}}>
-        <div style={{padding: 20, alignContent: 'center', width: 200}}>
-          <h3 style={{color: 'black', fontSize: 20}}>Hostname</h3>
-          <Select onChange={(x) => setHostname(x['value'])} options={hostnames} defaultValue={hostnames[0]} styles={{
-            option: (provided) => ({
-              ...provided,
-              color: 'black'
-            })
-          }}></Select>
-        </div>
-        <div style={{padding: 20, alignContent: 'center', width: 150}}>
-          <h3 style={{color: 'black', fontSize: 20}}>Device</h3>
-          <Select onChange={(x) => setDevice(x['value'])} options={devices} defaultValue={devices[0]} styles={{
-            option: (provided) => ({
-              ...provided,
-              color: 'black'
-            })
-          }}></Select>
+        <div style={{padding: 20}}>
+          <div style={{padding: 20, alignContent: 'center', width: 200}}>
+            <h3 style={{color: 'black', fontSize: 20}}>Hostname</h3>
+            <Select onChange={(x) => setHostname(x['value'])} options={hostnames} defaultValue={hostnames[0]} styles={{
+              option: (provided) => ({
+                ...provided,
+                color: 'black'
+              })
+            }}></Select>
+          </div>
+          <div style={{padding: 20, alignContent: 'center', width: 200}}>
+            <h3 style={{color: 'black', fontSize: 20}}>Device</h3>
+            <Select onChange={(x) => setDevice(x['value'])} options={devices} defaultValue={devices[0]} styles={{
+              option: (provided) => ({
+                ...provided,
+                color: 'black'
+              })
+            }}></Select>
+          </div>
+          <div style={{padding: 20, alignContent: 'center', width: 200}}>
+            <h3 style={{color: 'black', fontSize: 20}}>Statistic</h3>
+            <Select onChange={(x) => setStat(x['value'])} options={stats} defaultValue={stats[0]} styles={{
+              option: (provided) => ({
+                ...provided,
+                color: 'black'
+              })
+            }}></Select>
+          </div>
         </div>
         <div style={{padding: 30, paddingTop: 100}}>
-          <img src={'Hostnames\\' + hostname + '\\' + hostname + '_' + device + '_Temperature.png'}></img>
+          <img src={'Hostnames\\' + hostname + '\\' + hostname + '_' + device + '_' + stat + '.png'}></img>
         </div>
       </div>
     </>
